8bdfc29cc11f9e8a3de53f57729570360125e08f
bc_rikko <s.shin1106@gmail.com>
2015-08-03 15:03:12 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+Plot Editor
+===
+
+文章や歌詞を書くときに使えるプロットエディタです。
+
+<img alt="Plot Edidor" src="ploteditor.gif" width="400px">
+
+使用ライブラリ
+---
+
+* Vue.js 0.12.8
+* Sortable.js
+* Bootstrap 3.3.5
+
+使い方
+---
+
+|アクション|動作|
+| --- | --- |
+|「Add Plot」|プロットを1つ追加します|
+|「Export Plot」|プロットを出力します|
+|「Reset」|すべてを削除します|
+|Drag & Drop|プロットを移動します|
+|Ctrl+Enter|「Add Plot」と同じ|
+|Ctrl+s|「Export Plot」と同じ|
+|Ctrl+Del|現在のプロットを削除します|
+|Ctrl+↑|上のプロットに移動します|
+|Ctrl+↓|下のプロットに移動します|
+|Ctrl+Shift+↑|上のプロットと入れ替えます|
+|Ctrl+Shift+↓|下のプロットと入れ替えます|
+
+
+&copy; 2015 [Black Everyday Company](http://kuroeveryday.blogspot.com/)
---
+++ main.js
@@ -0,0 +1,113 @@
+var plots = [''];
+var index = 1;
+
+Vue.component('plot', {
+    template: '#plot-template',
+    replace: true,
+    props: ['plots'],
+    ready: function (value) {
+        var el = this.$el;
+
+        Sortable.create(el, {
+            draggable: 'li',
+            onUpdate: function(e) {
+                var oldPosition = e.item.getAttribute('data-id');
+                var newPosition = this.toArray().indexOf(oldPosition);
+
+                plots.splice(newPosition, 0, plots.splice(oldPosition, 1)[0]);
+            }
+        });          
+    },
+    methods: {
+        remove: function(index) {
+            plots.$remove(index);
+        },
+        shortcut: function (e, index) {
+            // shift + ctrl
+            if (e.$event.shiftKey && e.$event.ctrlKey) {
+                switch (e.$event.keyCode) {
+                    // up
+                    case 38:
+                        if (0 < index) {
+                            plots.splice(index - 1, 0, plots.splice(index, 1)[0]);
+                            document.getElementsByTagName('textarea')[index - 1].focus();
+                        }
+                        break;
+                    // down
+                    case 40:
+                        var el = document.getElementsByTagName('textarea');
+                        if (index < el.length - 1) {
+                            plots.splice(index + 1, 0, plots.splice(index, 1)[0]);
+                            document.getElementsByTagName('textarea')[index + 1].focus();
+                        }
+                        break;
+                }
+            }
+            // ctrl
+            else if (e.$event.ctrlKey) {
+                switch (e.$event.keyCode) {
+                    // enter
+                    case 13:
+                        main.add();
+                        break;
+                    // up
+                    case 38:
+                        if (0 < index) {
+                            document.getElementsByTagName('textarea')[index - 1].focus();
+                        }
+                        break;
+                    // down
+                    case 40:
+                        var el = document.getElementsByTagName('textarea');
+                        if (index < el.length - 1) {
+                            document.getElementsByTagName('textarea')[index + 1].focus();
+                        }
+                        break;
+                    // del
+                    case 46:
+                        this.remove(index);
+                        break;
+                    // s
+                    case 83:
+                        main.export();
+                        break;
+                }
+            }
+        }
+    }
+});
+
+var main = new Vue({
+    el: '#plot-editor',
+    data: {
+        plots: plots
+    },
+    methods: {
+        add: function() {
+            plots.push('');
+            
+            setTimeout(function() {
+                var e = document.getElementsByTagName('textarea');
+                e[e.length - 1].focus();
+            }, 100);
+
+        },
+        export: function() {
+            var text = plots.join('\r\n\r\n');
+            prompt("Export", text);
+        },
+        reset: function() {
+            if (confirm('プロットをリセットします。よろしいですか？')) {
+                var len = plots.length;
+                for (var i = 0; i < len; i++){
+                    plots.$remove(0);
+                }
+            }
+        }
+    }
+});
+
+// Ctrl+Sの無効化
+document.onkeydown = function () {
+    if (event.keyCode == 83) { return false;} 
+}
